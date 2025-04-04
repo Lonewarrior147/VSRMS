d3bc126d06b1a65976c72c1c60f0ee69dc1832a5
Prem Danasekaran <158161098+Black-Hawk-005@users.noreply.github.com>
2025-04-04 09:21:32 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,29 +1,94 @@
-from flask import Flask, render_template, request, redirect, url_for
-from models import db, Vehicle, Repair
+from flask import Flask, render_template, request, redirect, url_for, flash
+from models import db, Vehicle, User
 from datetime import datetime
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+from forms import LoginForm, RegisterForm
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login" 
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("Login successful!", "success")
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Invalid email or password", "danger")  # Flash message for invalid login
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    return redirect(url_for('view_vehicles'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        existing_email = User.query.filter_by(email=form.email.data).first()
+        if existing_email:
+            flash("This email is already registered.", "danger")
+            return redirect(url_for('register'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(email=form.email.data, password=hashed_password,
+                        name=form.name.data, phone=form.phone.data,
+                        address=form.address.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Account created successfully!", "success")
+        return redirect(url_for('login'))
+
+    
+    return render_template('register.html', form=form)
 
 @app.route('/add_vehicle', methods=['GET', 'POST'])
 def add_vehicle():
     if request.method == 'POST':
-        owner_name = request.form['owner_name']
         model = request.form['model']
+        year = request.form['year']  # Extract year from the form
+        odo_reading = request.form['odo_reading']  # Extract odometer reading from the form
         license_plate = request.form['license_plate']
-        purchase_date = datetime.strptime(request.form['purchase_date'],'%Y-%m-%d')
+        vin = request.form['vin']  # Extract VIN from the form
 
         existing_vehicle = Vehicle.query.filter_by(license_plate=license_plate).first()
 
         if existing_vehicle:
             return "Error: License plate already exists!", 400
-
-        vehicle = Vehicle(owner_name=owner_name, model=model, license_plate=license_plate, purchase_date=purchase_date)
+        
+        vehicle = Vehicle(user_id=current_user.id,
+                        model=model,
+                        year=year,  
+                        odo_reading=odo_reading,
+                        license_plate=license_plate,
+                        vin=vin)   
         db.session.add(vehicle)
         db.session.commit()
+        flash("Vehicle Added Successfully", "success")
 
         return redirect(url_for('view_vehicles'))
 
@@ -31,7 +96,7 @@ def add_vehicle():
 
 @app.route('/view_vehicles')
 def view_vehicles():
-    vehicles = Vehicle.query.all()
+    vehicles = Vehicle.query.filter_by(user_id=current_user.id).all()
     return render_template('view_vehicles.html', vehicles=vehicles)
 
 @app.route('/delete_vehicle/<int:id>')
@@ -40,6 +105,7 @@ def delete_vehicle(id):
     try:
         db.session.delete(vehicle_to_delete)
         db.session.commit()
+        
         return redirect(url_for('view_vehicles'))
     except:
         return "There was a error in deleting vehicle"
@@ -49,16 +115,18 @@ def update_vehicle(id):
     vehicle_to_update = Vehicle.query.get_or_404(id)
     
     if request.method == "POST":
-        vehicle_to_update.license_plate = request.form['license_plate']
-        vehicle_to_update.owner_name = request.form['owner_name']
+        vehicle_to_update.cid = request.form['cid']
         vehicle_to_update.model = request.form['model']
-        vehicle_to_update.purchase_date = request.form['purchase_date']
+        vehicle_to_update.year = int(request.form['year'])  # Convert to integer
+        vehicle_to_update.odo_reading = int(request.form['odo_reading'])  # Convert to integer
+        vehicle_to_update.license_plate = request.form['license_plate']
+        vehicle_to_update.vin = request.form['vin']
 
         try:
             db.session.commit()
             return redirect(url_for('view_vehicles'))
         
         except:
-            return 'These was an issue updating vehicle details'
-    else:
-        return render_template('update_vehicle.html', vehicle=vehicle_to_update)
+            return 'There was an issue updating vehicle details'
+    
+    return render_template('update_vehicle.html', vehicle=vehicle_to_update)
