95ddf92201317616393da5835574bdccee7a853d
Prem Danasekaran <158161098+Black-Hawk-005@users.noreply.github.com>
2025-04-04 09:19:37 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from flask_login import UserMixin
 db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    phone = db.Column(db.String(15), nullable=False)
+    address = db.Column(db.String(50), nullable=False)
+
+    vehicles_owned = db.relationship('Vehicle', backref='owner', lazy=True)
 
 class Vehicle(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner_name = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     model = db.Column(db.String(50), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    odo_reading = db.Column(db.Integer, nullable=False)
     license_plate = db.Column(db.String(20), unique=True, nullable=False)
-    purchase_date = db.Column(db.Date)
+    vin = db.Column(db.String(17), unique=True, nullable=False)
+
 
 class Repair(db.Model):
     id = db.Column(db.Integer, primary_key=True)
